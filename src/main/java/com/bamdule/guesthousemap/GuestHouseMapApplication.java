fc732ed4724fafa077eac52051b517de7783d152
minwookim <minwookim@tlab.co.kr>
2020-10-16 17:36:08 +0900

    spring boot 2.3.4 init

---
+++ src/main/java/com/bamdule/guesthousemap/GuestHouseMapApplication.java
@@ -0,0 +1,13 @@
+package com.bamdule.guesthousemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestHouseMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuestHouseMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bamdule/guesthousemap/GuesthousemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamdule.guesthousemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuesthousemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
